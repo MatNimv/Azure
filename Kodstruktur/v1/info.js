7e06d89ab2aae554c7104a6a8bfa196eea2ca1a5
Alex <zetterstrom.alexander@gmail.com>
2021-05-07 15:22:48 +0200

    Updated info.js
    Added functions to gather DB information :)

--- Kodstruktur/v1/info.js
+++ Kodstruktur/v1/info.js
@@ -1,12 +1,56 @@
 "use strict";
 
 CollectAllCityInfo();
-function CollectAllCityInfo(){
+function CollectAllCityInfo() {
 
     let cityArray = [];
+
+
     DB.CITIES.forEach(element => {
-        console.log(element.name);
-    });
+
+        let CityCountryCombo = DB.COUNTRIES.find(function (country) {
+            return country.id === element.countryID;
+        });
+
+        let RatingCityCombo = DB.COMMENTS_CITY.find(function (city) {
+            return city.id === element.id;
+        });
+
+        let UniversityCityCombo = DB.UNIVERSITIES.find(function (university) {
+            return university.id === element.id;
+        });
+
+        let EntertainmentCityCombo = DB.ENTERTAINMENT_PLACES.find(function (entertainment) {
+            return entertainment.id === element.id;
+        });
+
+      
+
+
+        let cityObject =
+        {
+            City: element.name,
+            Country: CityCountryCombo.name,
+            Flag: CityCountryCombo.flag,
+            StarsOut: RatingCityCombo.stars.out,
+            StarsFood: RatingCityCombo.stars.food,
+            StarsAccomodation: RatingCityCombo.stars.accomodation,
+            CityInfo: element.text,
+            Universities: UniversityCityCombo,
+            Images: CityCountryCombo.imagesNormal[0],
+            Entertainment: EntertainmentCityCombo
+        }
+
+
+
+    
+cityArray.push(cityObject)
+
+})
+
+console.log(cityArray)
+
+
     /*namn på Stad och Land + landets flagga
     rating för boende / mat / uteliv
     informationstext om staden
@@ -15,10 +59,10 @@ function CollectAllCityInfo(){
     en lista av alla aktiviteter i staden*/
 }
 
-function CollectAllProgramInfo(){
+function CollectAllProgramInfo() {
 
 }
 
-function CollectAllUniversityInfo(){
+function CollectAllUniversityInfo() {
 
 }
