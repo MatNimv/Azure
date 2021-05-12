bb3b70e31a6424ecb57a79b2b318d0c65c5c3df9
Alex <zetterstrom.alexander@gmail.com>
2021-05-12 17:52:27 +0200

    Changes to nav, footer and index

--- Kodstruktur/v1/index.js
+++ Kodstruktur/v1/index.js
@@ -1,34 +1,103 @@
 "use strict";
 
-AddWebsiteContent();
 
+
+
+
+//ImagePlay function
+
+let ImagePlayImageArray = [];
+let ImagePlayNameArray = [];
+
+AddWebsiteContent();
+//Add DOM element function
 function AddWebsiteContent() {
     let div = document.createElement("div");
     div.innerHTML =
         `
     <div id="ImageRollWrapper">
-    <div id="imageRollBackward">Previous Slide</div>
-    <div id="ImageRoll"></div>
-    <div id="imageRollPhotoForward">Next Slide</div>
+        <div id="ImageRollCell">
+            <div id="ImageRollBackward">Previous Slide</div>
+            <div id="ImageRoll"></div>
+            <div id="ImageRollPhotoForward">Next Slide</div>
+        </div>
+    <p id="NameOfImageRoll">ImagePlayNameArray[number]</p>
     </div>
     
-    <p id="NameOfImageRoll">NamnpåBild</p>
+    
     <div id="PluggaWrapper">
         <p>sugen på att plugga?</p>
         <button id="MainPageButton">Klicka här!</button>
+        
     </div>
     
-    <p>Se vad andra studender tycker!</p>
+  
 
     <div id="ReviewWrapper">
-        <div>review here</div>
-        <div>review here</div>
-        <div>review here</div>
-        <div>review here</div>
-        <div>review here</div>
-        <div>review here</div>
+      <p>Se vad andra studender tycker!</p>
+      <div id="ReviewCellWrapper">
+
+        <div class="ReviewCell">
+          <p id="ReviewProgramCity">Medical, Madrid</p>
+          <p id="ReviewText"> All is good</p>
+          <div id="ReviewNameDate">Sten, David, 10/02/2020</div>
+            <div id="ReviewStars">    
+               <div class="ReviewStarShapes"></div>
+               <div class="ReviewStarShapes"></div>
+               <div class="ReviewStarShapes"></div>
+            </div>
+  
+     </div>
+   
+     <div class="ReviewCell">
+          <p id="ReviewProgramCity">Medical, Madrid</p>
+          <p id="ReviewText"> All is good</p>
+          <div id="ReviewNameDate">Sten, David, 10/02/2020</div>
+            <div id="ReviewStars">    
+               <div class="ReviewStarShapes"></div>
+               <div class="ReviewStarShapes"></div>
+               <div class="ReviewStarShapes"></div>
+            </div>
+      
 </div>
+   
+   
+   
+
+
     `
 
     document.querySelector("main").append(div)
 }
+
+
+
+
+
+
+GetDBImages(DB);
+function GetDBImages(db, number = 0){
+  
+   
+    
+    db.CITIES.forEach(function(element){
+        ImagePlayImageArray.push(element.imagesNormal[0])
+    })
+
+    document.querySelector("#ImageRoll").style.backgroundImage = `url(ImagePlayImageArray[number])`;
+  
+}
+
+// NameOfImageRoll needs to change per city
+
+
+
+//Previous picture function
+document.querySelector("#ImageRollBackward").addEventListener("click", function () {
+    GetDBImages(DB, number)
+})
+
+//Next picture function
+document.querySelector("#ImageRollPhotoForward").addEventListener("click", function () {
+    GetDBImages(DB, number)
+})
